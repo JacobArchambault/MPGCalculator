e5aad69b98db1b20c17eeec9cc9c16fcd8d0d213
Jacob Archambault <jacobarchambault@gmail.com>
2021-02-08 13:05:30 -0500

    Initial commit

---
+++ src/com/jacobarchambault/propertytaxcalculator/PropertyTaxCalculator.java
@@ -0,0 +1,80 @@
+package com.jacobarchambault.propertytaxcalculator;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class PropertyTaxCalculator extends JFrame {
+	/**
+	 * CalcButtonListener is an action listener class for the calcButton component.
+	 */
+	private class CalcButtonListener implements ActionListener {
+		/**
+		 * actionPerformed method
+		 * 
+		 * @param e An ActionEvent object.
+		 */
+		@Override
+		public void actionPerformed(
+				ActionEvent e) {
+			propertyTaxPanel.showPropertyTax();
+		}
+	} // End of inner class
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+	private PropertyTaxPanel propertyTaxPanel; // A panel for displaying assessmentValue and tax
+
+	/**
+	 * Constructor
+	 */
+	public PropertyTaxCalculator(String title) {
+		super(title);
+		// Specify what happens when the close button is clicked.
+		setDefaultCloseOperation(
+				WindowConstants.EXIT_ON_CLOSE);
+		// Create a PropertyTaxPanel object.
+		propertyTaxPanel = new PropertyTaxPanel();
+		// Build the panel that contains the button.
+		buildButtonPanel();
+		// Add the panels to the content pane.
+		add(
+				propertyTaxPanel,
+				BorderLayout.CENTER);
+		// Pack and display the window.
+		pack();
+		setVisible(
+				true);
+	}
+
+	/**
+	 * The buildButtonPanel method creates a panel containing the calc property tax button.
+	 */
+	private void buildButtonPanel() {
+		// Create a button to calculate the property tax.
+		JButton calcButton = new JButton(
+				"Calculate Property tax");
+		// Add an action listener to the button.
+		calcButton.addActionListener(
+				new CalcButtonListener());
+		// Put the button in its own panel.
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(
+				calcButton);
+		add(
+				buttonPanel,
+				BorderLayout.SOUTH);
+
+	}
+
+}
---
+++ src/com/jacobarchambault/propertytaxcalculator/PropertyTaxPanel.java
@@ -0,0 +1,55 @@
+package com.jacobarchambault.propertytaxcalculator;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * The MinutesPanel class for use with the Long Distance Calls programming
+ * challenge.
+ */
+public class PropertyTaxPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField propertyValue; // To get property value
+
+	/**
+	 * Constructor
+	 */
+	public PropertyTaxPanel() {
+		// Create a text field.
+		propertyValue = new JTextField(
+				10);
+		// Create a GridLayout manager.
+		setLayout(
+				new GridLayout(
+						1,
+						2));
+		// Add the labels and text fields to this panel.
+		add(
+				new JLabel(
+						"Enter the actual property value:"));
+		add(
+				propertyValue);
+	}
+
+	public void showPropertyTax() {
+		// Get the assessmentValue.
+		double assessmentValue = Double.parseDouble(
+				propertyValue.getText()) * .6;
+		// Get the property tax.
+		double propertyTax = assessmentValue * .0064;
+		// Display them.
+		JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"Assessment Value: $%,.2f\nProperty tax: $%,.2f",
+						assessmentValue,
+						propertyTax));
+	}
+}
